a83ac1f2d4cc6528b42d7b52a3913289f6e1f365
Simon Smulders <simsmulders@gmail.com>
2019-01-04 23:52:31 +0100

    react and react-dom updated

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -26,7 +26,7 @@ class PostsNew extends Component {
 
   onSubmit(values) {
     this.props.createPost(values, () => {
-      this.props.history.push('/');
+      this.props.history.push('/events');
     });
   }
 
